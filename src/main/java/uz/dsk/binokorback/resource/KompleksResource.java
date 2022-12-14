555140c4ca267441f879f993f0786e979503992f
Akbar <akbar358888@gmail.com>
2022-12-14 13:15:34 +0500

    Merge remote-tracking branch 'origin'

--- src/main/java/uz/dsk/binokorback/resource/KompleksResource.java
+++ src/main/java/uz/dsk/binokorback/resource/KompleksResource.java
@@ -3,14 +3,21 @@ package uz.dsk.binokorback.resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import uz.dsk.binokorback.fileupload.FileService;
 import uz.dsk.binokorback.models.Dom;
 import uz.dsk.binokorback.models.Kompleks;
 import uz.dsk.binokorback.sevice.KompleksService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
+
+import static org.springframework.http.MediaType.parseMediaType;
 
 @Log4j2
 @RestController
@@ -46,69 +53,69 @@ public class KompleksResource {
     }
 
 
-//    @PostMapping(value = "upload", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
-//    public ResponseEntity<Boolean> uploadAndDownload(@RequestParam("id") String id,
-//                                                     @RequestParam("file") MultipartFile file,
-//                                                     @RequestParam("file0") MultipartFile file0,
-//                                                     @RequestParam("file1") MultipartFile file1) {
-//
-//
-//        Kompleks kompleks = kompleksService.getById(id);
-//        String filetype = file.getOriginalFilename();
-//        String filetype0 = file0.getOriginalFilename();
-//        String filetype1 = file1.getOriginalFilename();
-//
-//        boolean ans = false;
-//
-//        try {
-//            if (!file.isEmpty()) {
-//                kompleks.setMainimagepath(kompleks.getId() + "." + filetype.substring(filetype.lastIndexOf(".") + 1));
-//                if (fileService.uploadAndDownloadFile(file, "house", kompleks.getMainimagepath())) {
-//                    ans = true;
-////                final ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(fileService
-////                        .getFileStorageLocation() + "/" + file.getOriginalFilename())));
-//                }
-//            }
-//            if (!file0.isEmpty()) {
-//                kompleks.setMainimagepathfirst(kompleks.getId() + "-1." + filetype0.substring(filetype.lastIndexOf(".") + 1));
-//                if (fileService.uploadAndDownloadFile(file0, "house", kompleks.getMainimagepathfirst())) {
-//                    ans = true;
-//
-////                final ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(fileService
-////                        .getFileStorageLocation() + "/" + file.getOriginalFilename())));
-//                }
-//            }
-//            if (!file1.isEmpty()) {
-//                kompleks.setMainimagepathsecond(kompleks.getId() + "-2." + filetype1.substring(filetype.lastIndexOf(".") + 1));
-//                if (fileService.uploadAndDownloadFile(file1, "house", kompleks.getMainimagepathsecond())) {
-//                    ans = true;
-////                final ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(fileService
-////                        .getFileStorageLocation() + "/" + file.getOriginalFilename())));
-//                }
-//            }
-//            if (ans) {
-//                kompleksService.save(kompleks);
-//
-//                return ResponseEntity.ok().body(true);
-//            }
-//
-//            return ResponseEntity.ok().body(false);
-//        } catch (Exception e) {
-//            return ResponseEntity.ok().body(false);
-//        }
-//    }
-//
-//
-//    @GetMapping("download/house/{filename:.+}")
-//    public ResponseEntity<?> downloadFile(@PathVariable("filename") String filename) throws IOException {
-//
-//        Map map = fileService.DownloadFile(filename, "house");
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.IMAGE_PNG)
-//                .headers((HttpHeaders) map.get("headers"))
-//                .body(map.get("resource"));
-//
-//    }
+    @PostMapping(value = "upload")
+    public ResponseEntity<Boolean> uploadAndDownload(@RequestParam("id") String id,
+                                                     @RequestParam("file") MultipartFile file,
+                                                     @RequestParam("file0") MultipartFile file0,
+                                                     @RequestParam("file1") MultipartFile file1) {
+
+
+        Kompleks kompleks = kompleksService.getById(id);
+        String filetype = file.getOriginalFilename();
+        String filetype0 = file0.getOriginalFilename();
+        String filetype1 = file1.getOriginalFilename();
+
+        boolean ans = false;
+
+        try {
+            if (!filetype.isEmpty()) {
+                ResponseEntity.ok(fileService.storeFile(file, filetype, "house"));
+                ans = true;
+            }
+            if (!filetype0.isEmpty()) {
+                ResponseEntity.ok(fileService.storeFile(file0, filetype0, "house"));
+                ans = true;
+            }
+
+            if (!filetype1.isEmpty()) {
+                ResponseEntity.ok(fileService.storeFile(file1, filetype1, "house"));
+                ans = true;
+            }
+            if (ans) {
+                kompleksService.save(kompleks);
+
+                return ResponseEntity.ok().body(true);
+            }
+
+            return ResponseEntity.ok().body(false);
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(false);
+        }
+    }
+
+
+    @GetMapping("download/house/{filename:.+}")
+    public ResponseEntity<?> downloadFile(@PathVariable("filename") String filename, HttpServletRequest request) throws IOException {
+
+        Resource fileResource = fileService.getFile(filename, "models");
+
+        String contentType = null;
+
+        try {
+            contentType = request.getServletContext().getMimeType(fileResource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            log.error("Could not determine file type.");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
+                .body(fileResource);
+
+    }
 
 
 }
