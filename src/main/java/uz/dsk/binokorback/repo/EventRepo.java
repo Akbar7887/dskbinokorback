914fdae2bdf13c8802dc12475dd87b46da4c2790
Akbar <akbar358888@gmail.com>
2023-07-06 11:12:31 +0500

    Merge remote-tracking branch 'origin'

--- src/main/java/uz/dsk/binokorback/repo/EventRepo.java
+++ src/main/java/uz/dsk/binokorback/repo/EventRepo.java
@@ -1,11 +1,13 @@
 package uz.dsk.binokorback.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uz.dsk.binokorback.models.Event;
 
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
 
+    @Query("select 1, e from Event as e order by e.datecreate")
     Event getFirstById();
 }
